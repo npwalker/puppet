7a48f4556671725aee3cd23ec5d434be0ad0cd85
Nick Walker <nick.walker@puppetlabs.com>
2018-01-26 15:05:17 -0800

    (PUP-1916) Correctly revoke mulitple hosts when one has a CSR
    Prior to this commit, if you passed multiple hosts to
    puppet cert clean and one of the hosts had a CSR pending then
    none of the hosts would get their certs revoked but they
    would be destroyed.
    
    After this commit, we remove the hosts with a CSR from
    the list of hosts that should get their certs revoked.

--- lib/puppet/application/cert.rb
+++ lib/puppet/application/cert.rb
@@ -264,8 +264,9 @@ Copyright (c) 2011 Puppet Inc., LLC Licensed under the Apache 2.0 License
       hosts = command_line.args.collect { |h| h.downcase }
     end
     begin
-      if subcommand == :destroy and (@ca.waiting? & hosts).empty?
-        apply(@ca, :revoke, options.merge(:to => hosts))
+      if subcommand == :destroy
+        signed_hosts = hosts - @ca.waiting?
+        apply(@ca, :revoke, options.merge(:to => signed_hosts))
       end
       apply(@ca, subcommand, options.merge(:to => hosts, :digest => @digest))
     rescue => detail
